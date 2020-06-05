d0e87dd59aadf93731bcc7e1d488345194fefa40
t00thle55 <62895012+t00thle55@users.noreply.github.com>
2020-06-04 22:14:50 -0400

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,32 @@
+noun1 = input("Noun")
+wackyState = input("wHaCkY sTaTe")
+verb1 = input("Verb Past Tense")
+noun15 = input("Noun")
+noun2 = input("Noun")
+properName = input("Proper name")
+verb2 = input("verb")
+noun3 = input("Noun")
+verb3 = input("Verb")
+noun4 = input("Noun")
+partOfTheBody = input("Body Part")
+adjective = input("Adjective")
+relative = input("Relative")
+activity = input("Activity")
+chainRestaurant = input("Chain Restaurant")
+adjectivePastTense = input("Adjective Past Tense")
+month = input("Month")
+verb4 = input("Verb")
+noun5 = input("Noun")
+verbPastTense = input("Verb Past Tense")
+adjective2 = input("Adjective")
+verb5 = input("Verb")
+noun6 = input("Noun")
+pluralNoun = input("Plural Noun")
+
+print("A " + noun1 + " in " + wackyState + " was arrested this morning after he " + verb1 + " a " + noun15 + " in front of a " +
+      noun2 + ". " + properName + ", had a history of " + verb2 + ", but no one-- not even his "+ noun3 + "-- ever imagined he\'d " +
+      verb3 + " with a " + noun4 + " stuck in his " + partOfTheBody + ". \"I always thought he was " + adjective + ", but I never thought he'd do something like this. Even his "
+      + relative + " was surprised.\" After a brief " + activity + ", cops followed him to a " + chainRestaurant + ", where he reportedly " + adjectivePastTense +
+      " in the fry machine. In " + month + ", a woman was charged with a similar crime, but rather than " + verb4 + " with a " + noun5 + ", she " + verbPastTense + " with a " + adjective2 +
+      " dog. Either way, we imagine that after witnessing him " + verb5 + " with a " + noun6 + ", there are probably a whole lot of " + pluralNoun + " that are going to need some therapy.")
+
